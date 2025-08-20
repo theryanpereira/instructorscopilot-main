6ec1111b962e5854c4e9339b7e3ce1b9d9fb31ec
Sivakoti Satwik <sivakotisatwik2006@gmail.com>
2025-08-20 21:16:49 +0530

    added the shell file which helps the render to run the backend in their servers

---
+++ backend/start.sh
@@ -0,0 +1,12 @@
+echo "Starting AI Copilot for Instructors..."
+python llm.py
+echo "Master instructions generated."
+echo "Starting agents"
+cd copilot
+python main.py
+python deep_main.py
+cd ..
+python course_material.py
+python quizzes.py
+python flash_cards.py
+python ppt.py
